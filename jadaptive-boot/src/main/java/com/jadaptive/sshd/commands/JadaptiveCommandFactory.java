198ac497bf1e1deeeb3ba301cf6cc8790c73367f
ludup <lee@ludup.com>
2019-11-16 18:21:15 +0000

    New users command.

--- jadaptive-boot/src/main/java/com/jadaptive/sshd/commands/JadaptiveCommandFactory.java
+++ jadaptive-boot/src/main/java/com/jadaptive/sshd/commands/JadaptiveCommandFactory.java
@@ -12,10 +12,8 @@ import com.jadaptive.permissions.PermissionService;
 import com.jadaptive.user.UserService;
 import com.sshtools.common.permissions.PermissionDeniedException;
 import com.sshtools.common.ssh.SshConnection;
-import com.sshtools.server.vsession.Command;
 import com.sshtools.server.vsession.CommandFactory;
 import com.sshtools.server.vsession.ShellCommand;
-import com.sshtools.server.vsession.VirtualConsole;
 
 @Component
 public class JadaptiveCommandFactory extends CommandFactory<ShellCommand> {
@@ -34,6 +32,8 @@ public class JadaptiveCommandFactory extends CommandFactory<ShellCommand> {
 		installCommand("passwd", Passwd.class);
 		installCommand("roles", Roles.class);
 		installCommand("tenants", Tenants.class);
+		installCommand("permissions", Permissions.class);
+		installCommand("users", Users.class);
 	}
 	
 	protected void configureCommand(ShellCommand command, SshConnection con) throws IOException, PermissionDeniedException {
---
+++ jadaptive-boot/src/main/java/com/jadaptive/sshd/commands/Users.java
@@ -0,0 +1,53 @@
+package com.jadaptive.sshd.commands;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jadaptive.permissions.PermissionService;
+import com.jadaptive.role.Role;
+import com.jadaptive.user.User;
+import com.jadaptive.user.UserService;
+import com.sshtools.common.permissions.PermissionDeniedException;
+import com.sshtools.server.vsession.CliHelper;
+import com.sshtools.server.vsession.UsageException;
+import com.sshtools.server.vsession.UsageHelper;
+import com.sshtools.server.vsession.VirtualConsole;
+
+public class Users extends UserCommand {
+
+	@Autowired
+	PermissionService permissionService; 
+	
+	@Autowired
+	UserService userService; 
+	
+	public Users() {
+		super("users", "User Management", UsageHelper.build("users [option] [user]",
+				"-l, --list                           List all users"),
+				"List all permissions");
+	}
+
+	@Override
+	protected void doRun(String[] args, VirtualConsole console)
+			throws IOException, PermissionDeniedException, UsageException {
+		
+		if(CliHelper.hasLongOption(args, "help")) {
+			printUsage();
+		} else if(args.length==1 || CliHelper.hasShortOption(args, 'l') || CliHelper.hasLongOption(args, "list")) {	
+			printUsers();
+		} else {
+			console.println("Invalid arguments!");
+			printUsage();
+		}
+	}
+
+	private void printUsers() {
+		for(User user : userService.iterateUsers()) {
+			console.println(user.getUsername());
+		}
+	}
+}
