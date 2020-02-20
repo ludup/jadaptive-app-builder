63be14d0753be8cabd0f2ea932369fa92761f1e5
Lee Painter <lee@ludup.com>
2020-02-20 23:03:15 +0000

    Removed example plugin.

--- plugins/example-plugin/src/main/java/com/jadaptive/plugins/example/ExampleEntity.java
+++
@@ -1,7 +0,0 @@
-package com.jadaptive.plugins.example;
-
-import com.jadaptive.api.repository.NamedUUIDEntity;
-
-public class ExampleEntity extends NamedUUIDEntity {
-
-}
--- plugins/example-plugin/src/main/java/com/jadaptive/plugins/json/servlet/PluginControllerConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.jadaptive.plugins.json.servlet;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
-
-@Configuration
-public class PluginControllerConfiguration extends DelegatingWebMvcConfiguration {
-
-}
