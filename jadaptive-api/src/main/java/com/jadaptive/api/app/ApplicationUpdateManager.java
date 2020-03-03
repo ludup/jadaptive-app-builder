405e5f5bdc7e118ac658e5992c1257d85b9527d3
Lee Painter <lee@ludup.com>
2020-03-03 20:56:36 +0000

    New update manager.

--- jadaptive-api/src/main/java/com/jadaptive/api/app/ApplicationUpdateManager.java
+++ jadaptive-api/src/main/java/com/jadaptive/api/app/ApplicationUpdateManager.java
@@ -7,11 +7,13 @@ import org.pf4j.update.PluginInfo;
 
 public interface ApplicationUpdateManager {
 
-	boolean installUpdates(List<PluginInfo> toUpdate, List<PluginInfo> toInstall) throws IOException;
-
 	boolean installUpdates() throws IOException;
 
 	void check4Updates(List<PluginInfo> toUpdate, List<PluginInfo> toInstall);
+
+	void restart();
+
+	void shutdown();
 
 
 }
