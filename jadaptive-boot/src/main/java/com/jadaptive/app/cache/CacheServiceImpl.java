23e29a79b31a90a8581cb37f87f71d11efc5feac
Lee Painter <lee@javassh.com>
2020-05-25 13:14:55 +0100

    Revert CacheService to previous implementation.

--- jadaptive-boot/src/main/java/com/jadaptive/app/cache/CacheServiceImpl.java
+++ jadaptive-boot/src/main/java/com/jadaptive/app/cache/CacheServiceImpl.java
@@ -2,7 +2,6 @@ package com.jadaptive.app.cache;
 
 import javax.cache.Cache;
 import javax.cache.CacheManager;
-import javax.cache.Caching;
 import javax.cache.configuration.CompleteConfiguration;
 import javax.cache.configuration.Factory;
 import javax.cache.configuration.MutableConfiguration;
@@ -12,13 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jadaptive.api.cache.CacheService;
-import com.jadaptive.api.db.ClassLoaderService;
 
 @Service
 public class CacheServiceImpl implements CacheService {
 
 	@Autowired
-	private ClassLoaderService classLoaderService;
+	private CacheManager cacheManager;
+	
 	
 	public <K,V> Cache<K, V> getCacheOrCreate(String name,Class<K> key, Class<V> value){
 		return cache(name, key, value, baseConfiguration(key, value));
@@ -29,12 +28,11 @@ public class CacheServiceImpl implements CacheService {
 	}
 	
 	public <K,V> Cache<K, V> getCacheIfExists(String name, Class<K> key, Class<V> value){
-		CacheManager cacheManager = Caching.getCachingProvider(classLoaderService.getClassLoader()).getCacheManager();
 		return cacheManager.getCache(name,key,value);
 	}
 	
 	public CacheManager getCacheManager(){
-		return Caching.getCachingProvider(classLoaderService.getClassLoader()).getCacheManager();
+		return this.cacheManager;
 	}
 	
 	private <K,V> CompleteConfiguration<K, V> baseConfiguration(Class<K> key, Class<V> value){
@@ -42,7 +40,6 @@ public class CacheServiceImpl implements CacheService {
 	} 
 	
 	private <K,V> Cache<K, V> cache(String name, Class<K> key, Class<V> value, CompleteConfiguration<K, V> config){
-		CacheManager cacheManager = Caching.getCachingProvider(classLoaderService.getClassLoader()).getCacheManager();
 		Cache<K, V> cache = cacheManager.getCache(name,key,value);
 		return cache == null ? cacheManager.createCache(name, config) : cache;
 	}
