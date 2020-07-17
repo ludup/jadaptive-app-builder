41925de8ec916cb01e47be148b36dfc25f5f33e2
Lee Painter <lee@ludup.com>
2020-07-17 15:33:13 +0100

    New remote users plugin

---
+++ plugins/jadaptive-remote-users/src/main/java/com/jadaptive/plugins/remote/RemoteUsersPlugin.java
@@ -0,0 +1,13 @@
+package com.jadaptive.plugins.remote;
+
+import org.pf4j.PluginWrapper;
+
+import com.jadaptive.api.spring.AbstractSpringPlugin;
+
+public class RemoteUsersPlugin extends AbstractSpringPlugin {
+
+	public RemoteUsersPlugin(PluginWrapper wrapper) {
+		super(wrapper);
+	}
+
+}
