654ec79f3f606a074c89eb1b080ad08c7d99417e
Lee Painter <lee@ludup.com>
2020-08-20 14:30:25 +0100

    Stop the logger.

--- plugins/jadaptive-ssh-server/src/main/java/com/jadaptive/plugins/sshd/SSHDServerPlugin.java
+++ plugins/jadaptive-ssh-server/src/main/java/com/jadaptive/plugins/sshd/SSHDServerPlugin.java
@@ -3,10 +3,20 @@ package com.jadaptive.plugins.sshd;
 import org.pf4j.PluginWrapper;
 
 import com.jadaptive.api.spring.AbstractSpringPlugin;
+import com.sshtools.common.logger.DefaultLoggerContext;
+import com.sshtools.common.logger.Log;
 
 public class SSHDServerPlugin extends AbstractSpringPlugin {
 	
 	public SSHDServerPlugin(PluginWrapper wrapper) {
 		super(wrapper);
 	}
+
+	@Override
+	public void stop() {
+		Log.getDefaultContext().shutdown();
+		super.stop();
+	}
+	
+	
 }
