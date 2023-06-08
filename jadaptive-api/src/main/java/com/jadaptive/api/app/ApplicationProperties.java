bdd430c9afdfda3213f9dc1c6ad58f0240602e01
Lee Painter <lee@javassh.com>
2023-06-08 17:17:21 +0100

    Remove console off default logging config

--- jadaptive-api/src/main/java/com/jadaptive/api/app/ApplicationProperties.java
+++ jadaptive-api/src/main/java/com/jadaptive/api/app/ApplicationProperties.java
@@ -93,9 +93,6 @@ public class ApplicationProperties {
         </Property>
     </Properties>
     <Appenders>
-        <Console name="ConsoleAppender" target="SYSTEM_OUT">
-            <PatternLayout pattern="${LOG_PATTERN}"/>
-        </Console>
 		<RollingFile name="FileAppender" fileName="logs/application.log"
 		         filePattern="application-%i.log.gz">
 		    <PatternLayout>
@@ -110,7 +107,6 @@ public class ApplicationProperties {
     <Loggers>
         <Root level="info">
             <AppenderRef ref="FileAppender" />
-            <AppenderRef ref="ConsoleAppender"/>
         </Root>
     </Loggers>
 </Configuration>
