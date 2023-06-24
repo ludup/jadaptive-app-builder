0884f25513af15f8c41d18b4be66f8ab137ffda1
Lee Painter <lee@javassh.com>
2023-06-24 15:45:37 +0100

    Added FixedNamedUUIDEntity for read only names.

---
+++ jadaptive-api/src/main/java/com/jadaptive/api/repository/FixedNamedUUIDEntity.java
@@ -0,0 +1,23 @@
+package com.jadaptive.api.repository;
+
+import com.jadaptive.api.template.FieldType;
+import com.jadaptive.api.template.ObjectField;
+import com.jadaptive.api.template.ValidationType;
+import com.jadaptive.api.template.Validator;
+
+public abstract class FixedNamedUUIDEntity extends AbstractUUIDEntity implements NamedDocument {
+
+	private static final long serialVersionUID = 2690511827179922811L;
+
+	@ObjectField(searchable = true, unique = true, type = FieldType.TEXT, nameField = true, readOnly = true)
+	@Validator(type = ValidationType.REQUIRED)
+	protected String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
