548f878311bf3686a3cb865d9a20480e14d5cffd
Lee Painter <lee@javassh.com>
2024-01-08 21:06:42 +0000

    Extended constructors of TextFormInput and added EmailFormInput.

--- jadaptive-api/src/main/java/com/jadaptive/api/ui/renderers/form/EmailFormInput.java
+++ jadaptive-api/src/main/java/com/jadaptive/api/ui/renderers/form/EmailFormInput.java
@@ -3,20 +3,20 @@ package com.jadaptive.api.ui.renderers.form;
 import com.jadaptive.api.template.ObjectTemplate;
 import com.jadaptive.api.template.TemplateViewField;
 
-public class TextFormInput extends FormInputRender {
+public class EmailFormInput extends TextFormInput {
 
-	public TextFormInput(ObjectTemplate template, TemplateViewField field) {
+	public EmailFormInput(ObjectTemplate template, TemplateViewField field) {
 		super(template, field);
 	}
 
-	public TextFormInput(ObjectTemplate template, String resourceKey, String formVariable, String bundle) {
+	public EmailFormInput(ObjectTemplate template, String resourceKey, String formVariable, String bundle) {
 		super(template, resourceKey, formVariable, bundle);
 	}
 
-
 	@Override
 	public String getInputType() {
-		return "text";
+		return "email";
 	}
 
+	
 }
