89a9f2886ad28d28f557a5cbcdb3efe151b67b75
ludup <lee@logonbox.com>
2024-02-02 23:23:48 +0000

    Support for sending files via browser.

--- jadaptive-api/src/main/resources/com/jadaptive/api/upload/UploadWidget.js
+++ jadaptive-api/src/main/resources/com/jadaptive/api/upload/UploadWidget.js
@@ -119,95 +119,104 @@ var UploadWidget = {
 
 			$(_self._options.feedbackDiv + ' .alert').remove();
 
-			if(_self._options.validate) {
-				if(!_self._options.validate()) {
+			var doUpload = function(onComplete) {
+				if($('.file-input').length <= 1) {
+					$(_self._options.feedbackDiv).prepend('<p class="alert alert-danger"><i class="' + $('body').data('iconset') + ' fa-exclamation-circle"></i> There are no files selected!</p>');
 					return;
 				}
-			}			
-			if($('.file-input').length <= 1) {
-				$(_self._options.feedbackDiv).prepend('<p class="alert alert-danger"><i class="' + $('body').data('iconset') + ' fa-exclamation-circle"></i> There are no files selected!</p>');
-				return;
-			}
-			$('.file-input').last().remove();
-			
-			JadaptiveUtils.startAwesomeSpin($('#uploadButton i'), 'fa-upload');
-
-			$('#uploadProgress').addClass('uploading');
-
-			var fd = new FormData();
-			if(_self._options.callback) {
-				_self._options.callback(fd);
-			}
-			
-			var countFiles = 0;
-			$('.file-input').each(function(idx, file) {
-				for(i=0;i<this.files.length;i++) {
-					if($("[data-filename='" + this.files[i].name + "']").length > 0) {
-						fd.append('file', this.files[i]);
-						countFiles++;
-					}
-				}
-			});
-		    
-			$('#progressBar').css("width", 0).attr('aria-valuenow', "0");
-			$('#helpText').hide(); 
-			$('#uploadButton').attr('disabled', 'disabled');
-			$('#uploadProgress').removeClass('d-none');
-		    $.ajax({
-		           type: "POST",
-		           url: _self._options.postUrl,
-		           dataType: "json",
-		           contentType: false,
-		           processData: false,
-		           data: fd,
-		           success: function(data)
-		           {
-		        	   if(data.success) {
-					       
-							if(complete) {
-								if(complete()) {
-									return;
-								}
-							}
-							
-							if(!_self._options.disableFeedback) {
-								$(_self._options.feedbackDiv).prepend('<p class="alert alert-success"><i class="' + $('body').data('iconset') + ' fa-check"></i> The upload of ' + countFiles + ' file(s) completed.</p>');		
-							}
-//							setTimeout(function() {
-								JadaptiveUtils.stopAwesomeSpin($('#uploadButton i'), 'fa-upload');
-								if(_self._options.successUrl) {
-									window.location = _self._options.successUrl;
-								}
-//							}, 2000);
- 
-		        	   } else {
-		        		   $(_self._options.feedbackDiv).prepend('<p class="alert alert-danger"><i class="' + $('body').data('iconset') + ' fa-exclamation-circle"></i> ' + data.message + '</p>');
-							_self.clearFiles();
-		        	   }
-		           },
-		           complete: function() {
-		        	    JadaptiveUtils.stopAwesomeSpin($('#uploadButton i'), 'fa-upload');
-					    $('#uploadButton').attr('disabled', false);
-						$('#uploadProgress').addClass('d-none');
-						$('#uploadProgress').removeClass('uploading');
-						$('.file-index').remove();
-						$('#files').append('<input class="file-input d-none" type="file" name="file" multiple/>');
-						$('#helpText').show(); 
-		           },
-				   xhr: function() {
-				        var xhr = new window.XMLHttpRequest();
+				$('.file-input').last().remove();
 				
-				        // Upload progress
-				        xhr.upload.addEventListener("progress", function(evt){
-				            if (evt.lengthComputable) {
-				                var percentComplete = Math.round((evt.loaded / evt.total) * 100);
-							  $('#progressBar').css('width', percentComplete + "%").attr('aria-valuenow', percentComplete);
-				            }
-				       }, false);
-				       
-				       return xhr;
-				    }
-		     });
+				JadaptiveUtils.startAwesomeSpin($('#uploadButton i'), 'fa-upload');
+	
+				$('#uploadProgress').addClass('uploading');
+	
+				var fd = new FormData();
+				if(_self._options.callback) {
+					_self._options.callback(fd);
+				}
+				
+				var countFiles = 0;
+				$('.file-input').each(function(idx, file) {
+					for(i=0;i<this.files.length;i++) {
+						if($("[data-filename='" + this.files[i].name + "']").length > 0) {
+							fd.append('file', this.files[i]);
+							countFiles++;
+						}
+					}
+				});
+			    
+				$('#progressBar').css("width", 0).attr('aria-valuenow', "0");
+				$('#helpText').hide(); 
+				$('#uploadButton').attr('disabled', 'disabled');
+				$('#uploadProgress').removeClass('d-none');
+			    $.ajax({
+			           type: "POST",
+			           url: _self._options.postUrl,
+			           dataType: "json",
+			           contentType: false,
+			           processData: false,
+			           data: fd,
+			           success: function(data)
+			           {
+			        	   if(data.success) {
+						       
+								if(complete) {
+									if(complete()) {
+										return;
+									}
+								}
+								
+								if(onComplete) {
+									onComplete();
+								}
+								
+								if(!_self._options.disableFeedback) {
+									$(_self._options.feedbackDiv).prepend('<p class="alert alert-success"><i class="' + $('body').data('iconset') + ' fa-check"></i> The upload of ' + countFiles + ' file(s) completed.</p>');		
+								}
+	//							setTimeout(function() {
+									JadaptiveUtils.stopAwesomeSpin($('#uploadButton i'), 'fa-upload');
+									if(_self._options.successUrl) {
+										window.location = _self._options.successUrl;
+									}
+	//							}, 2000);
+	 
+			        	   } else {
+			        		   $(_self._options.feedbackDiv).prepend('<p class="alert alert-danger"><i class="' + $('body').data('iconset') + ' fa-exclamation-circle"></i> ' + data.message + '</p>');
+								_self.clearFiles();
+			        	   }
+			           },
+			           complete: function() {
+			        	    JadaptiveUtils.stopAwesomeSpin($('#uploadButton i'), 'fa-upload');
+						    $('#uploadButton').attr('disabled', false);
+							$('#uploadProgress').addClass('d-none');
+							$('#uploadProgress').removeClass('uploading');
+							$('.file-index').remove();
+							$('#files').append('<input class="file-input d-none" type="file" name="file" multiple/>');
+							$('#helpText').show(); 
+			           },
+					   xhr: function() {
+					        var xhr = new window.XMLHttpRequest();
+					
+					        // Upload progress
+					        xhr.upload.addEventListener("progress", function(evt){
+					            if (evt.lengthComputable) {
+					                var percentComplete = Math.round((evt.loaded / evt.total) * 100);
+								  $('#progressBar').css('width', percentComplete + "%").attr('aria-valuenow', percentComplete);
+					            }
+					       }, false);
+					       
+					       return xhr;
+					    }
+			     });
+			}
+			if(_self._options.validate) {
+				if(!_self._options.validate(doUpload)) {
+					return;
+				}
+			}	
+			
+			doUpload();		
+			
     }
 };
 
