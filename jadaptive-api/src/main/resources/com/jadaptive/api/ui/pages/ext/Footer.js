bed75c9d51138798c08c2fffbc50bc21cc6077f6
ludup <lee@logonbox.com>
2024-02-07 22:29:13 +0000

    copyLink class for easy copying of URLs

--- jadaptive-api/src/main/resources/com/jadaptive/api/ui/pages/ext/Footer.js
+++ jadaptive-api/src/main/resources/com/jadaptive/api/ui/pages/ext/Footer.js
@@ -415,6 +415,13 @@ $(function() {
 		});
 	});
 	
+	$('.copyLink').click(function(e) {
+		e.preventDefault();
+		var copyText = $(this).attr('href');
+	  	navigator.clipboard.writeText(copyText);
+		$(this).append($('<span class="ms-3 text-success text-decoration-none"><sup>${default:copied.text}</sup></span>').fadeOut(2000));
+	});
+	
 	$('.removeAction').on('click', function(e) {
 		e.preventDefault();
 		var name = $(this).data('name');
