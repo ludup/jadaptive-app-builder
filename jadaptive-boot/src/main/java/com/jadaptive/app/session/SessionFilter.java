27e8d1b4373250166c684bb8491b64d51eec2a86
Brett Smith <brett@logonbox.com>
2024-06-13 23:54:21 +0100

    I could not get the tenant signup redirection to work as I would expect, and misconfiguration could cause endless loops. I have rearranged the algorithm so that https://signup.domain for example can be called without setting up redirection and added some other safe guards to prevent endless looping.
     * Check for registration domain takes precedence over require valid domain.
     * Do not do any redirection if the hostname is the local host (i.e. no tenant)
     * If require  valid domain is set, only redirect to invalid domain redirect if it is set
     * If it is not set, redirect to the root domain, if itself is set
     * If that is not set, then return a 404.

--- jadaptive-boot/src/main/java/com/jadaptive/app/session/SessionFilter.java
+++ jadaptive-boot/src/main/java/com/jadaptive/app/session/SessionFilter.java
@@ -2,6 +2,8 @@ package com.jadaptive.app.session;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.URI;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
@@ -100,24 +102,40 @@ public class SessionFilter implements Filter {
 			Tenant tenant = tenantService.getCurrentTenant();
 
 			if(Objects.nonNull(tenant)) {
-				if(!tenant.isValidHostname(request.getServerName())) {
+				if(!tenant.isValidHostname(request.getServerName()) && !req.getServerName().equals(InetAddress.getLocalHost().getHostName()) ) {
 					TenantConfiguration config = tenantConfig.getObject(TenantConfiguration.class);
-					if(config.getRequireValidDomain()) {
-						if(req.getServerName().equalsIgnoreCase(config.getRegistrationDomain())) {
-							if(req.getRequestURI().equals("/")) {
-								if(req.getServerPort() != -1 && req.getServerPort() != 443) {
-									resp.sendRedirect(String.format("https://%s:%d/app/ui/wizards/setupTenant", 
-											config.getRegistrationDomain(),
-											request.getServerPort()));
-								} else {
-									resp.sendRedirect(String.format("https://%s/app/ui/wizards/setupTenant", config.getRegistrationDomain()));
-								}	
-								return;
+					if(req.getServerName().equalsIgnoreCase(config.getRegistrationDomain())) {
+						if(req.getRequestURI().equals("/")) {
+							if(req.getServerPort() != -1 && req.getServerPort() != 443) {
+								resp.sendRedirect(String.format("https://%s:%d/app/ui/wizards/setupTenant", 
+										config.getRegistrationDomain(),
+										request.getServerPort()));
+							} else {
+								resp.sendRedirect(String.format("https://%s/app/ui/wizards/setupTenant", config.getRegistrationDomain()));
+							}	
+							return;
+						}
+					} else {
+						if(config.getRequireValidDomain()) {
+							String redir;
+							if(StringUtils.isBlank(config.getInvalidDomainRedirect())) {
+								if(StringUtils.isBlank(config.getRootDomain())) {
+									resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+									return;
+								}
+								else {
+									redir = "https://" + config.getRootDomain();	
+								}
+							}
+							else {
+								redir = config.getInvalidDomainRedirect();
 							}
 							
-						} else {
-							resp.sendRedirect(config.getInvalidDomainRedirect());
-							return;
+							URI redirUri = URI.create(redir);
+							if(!redirUri.getHost().equals(req.getServerName())) {
+								resp.sendRedirect(redir);
+								return;
+							}
 						}
 					}
 				}
