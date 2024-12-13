20ea710a4a4c2754670dadd79d9ad669f0961df7
Brett Smith <brett@logonbox.com>
2024-12-13 12:52:35 +0000

    Technique used for getting the "list" of resolvers (there is actually only one), does not work due to bean initialisation order. BootstrapThemeServiceImpl is in the core as is constructed before any `BootstrapThemeResolver` instances are constructed, so the list will always be null. Instead these are now looked up at run-time. Perhaps they should be anyway (API tweaks would be needed for this to be useful). Revisit if and when there is more than one resolver.
    The performance impact of this *should* be minimal.

--- jadaptive-boot/src/main/java/com/jadaptive/app/ui/BootstrapThemeServiceImpl.java
+++ jadaptive-boot/src/main/java/com/jadaptive/app/ui/BootstrapThemeServiceImpl.java
@@ -1,15 +1,11 @@
 package com.jadaptive.app.ui;
 
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jadaptive.api.app.ApplicationService;
 import com.jadaptive.api.ui.pages.ext.BootstrapTheme;
 import com.jadaptive.api.ui.pages.ext.BootstrapThemeResolver;
 import com.jadaptive.api.ui.pages.ext.BootstrapThemeService;
@@ -18,24 +14,16 @@ import com.jadaptive.api.ui.pages.ext.Footer;
 @Service
 public class BootstrapThemeServiceImpl implements BootstrapThemeService {
 
-	@Autowired(required = false)
-	List<BootstrapThemeResolver> resolvers;
-	
-	@PostConstruct
-	private void postConstruct() {
-		if(Objects.nonNull(resolvers)) {
-			Collections.sort(resolvers, Comparator.comparingInt(o -> o.weight()));
-		}
-	}
+	@Autowired
+	private ApplicationService applicationService;
 	
 	@Override
 	public BootstrapTheme getTheme() {
-		
-		if(Objects.isNull(resolvers)) {
-			return Footer.getThemeFromCookie(BootstrapTheme.DEFAULT);
-		}
-		
-		return resolvers.iterator().next().getTheme();
+		return applicationService.getBeans(BootstrapThemeResolver.class).stream().
+				sorted(Comparator.comparingInt(o -> o.weight())).
+				findFirst().
+				map(BootstrapThemeResolver::getTheme).
+				orElseGet(() -> Footer.getThemeFromCookie(BootstrapTheme.DEFAULT));
 	}
 
 }
