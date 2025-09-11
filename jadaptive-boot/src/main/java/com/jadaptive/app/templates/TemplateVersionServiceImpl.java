7fec2cd081617b6d5766d254cf091250b4b5f385
Lee Painter <lee@javassh.com>
2025-09-11 10:03:46 +0100

    Revert "Some useful methods to execute as a user on a specific tenant."
    This reverts commit 610f26ba03399614f3a733aab597a3e3aa86e9db.

--- jadaptive-boot/src/main/java/com/jadaptive/app/templates/TemplateVersionServiceImpl.java
+++ jadaptive-boot/src/main/java/com/jadaptive/app/templates/TemplateVersionServiceImpl.java
@@ -116,7 +116,6 @@ import net.bytebuddy.implementation.FieldAccessor;
 import net.bytebuddy.implementation.FixedValue;
 import net.bytebuddy.implementation.MethodCall;
 import net.bytebuddy.matcher.ElementMatchers;
-import static net.bytebuddy.matcher.ElementMatchers.*;
 
 @Service
 public class TemplateVersionServiceImpl extends AbstractLoggingServiceImpl implements TemplateVersionService  {
@@ -730,37 +729,37 @@ public class TemplateVersionServiceImpl extends AbstractLoggingServiceImpl imple
 		
 		generateEventTemplate(StringUtils.capitalize(group) + "Created",
 				template.getResourceKey(), template.getBundle(), group, clz, 
-				Events.created(template.getResourceKey()), newSchema, true, true);
+				Events.created(template.getResourceKey()), newSchema, true);
 		
 		generateUpdateEventTemplate(StringUtils.capitalize(group) + "Updated", 
 				template.getResourceKey(), template.getBundle(), group, clz, 
-				Events.updated(template.getResourceKey()), newSchema, true, true);
+				Events.updated(template.getResourceKey()), newSchema, true);
 		
 		generateEventTemplate(StringUtils.capitalize(group) + "Deleted",
 				template.getResourceKey(), template.getBundle(), group, clz, 
-				Events.deleted(template.getResourceKey()), newSchema, true, true);
+				Events.deleted(template.getResourceKey()), newSchema, true);
 		
 		generateEventTemplate(StringUtils.capitalize(group) + "Stashed",
 				template.getResourceKey(), template.getBundle(), group, clz, 
-				Events.stashed(template.getResourceKey()), newSchema, false, true);
+				Events.stashed(template.getResourceKey()), newSchema, false);
 		
 		generateEventTemplate(StringUtils.capitalize(group) + "Creating",
 				template.getResourceKey(), template.getBundle(), group, clz, 
-				Events.creating(template.getResourceKey()), newSchema, false, false);
+				Events.creating(template.getResourceKey()), newSchema, false);
 		
 		generateUpdateEventTemplate(StringUtils.capitalize(group) + "Updating", 
 				template.getResourceKey(), template.getBundle(), group, clz, 
-				Events.updating(template.getResourceKey()), newSchema, false, false);
+				Events.updating(template.getResourceKey()), newSchema, false);
 		
 		generateEventTemplate(StringUtils.capitalize(group) + "Deleting",
 				template.getResourceKey(), template.getBundle(), group, clz, 
-				Events.deleting(template.getResourceKey()), newSchema, false, false);
+				Events.deleting(template.getResourceKey()), newSchema, false);
 		
 	}
 	
 	@SuppressWarnings("unchecked")
 	private void generateEventTemplate(String className, String resourceKey, String bundle, String group, Class<?> clz, 
-			String eventKey, boolean newSchema, boolean audited, boolean async) {
+			String eventKey, boolean newSchema, boolean audited) {
 		
 		Generic genericType = TypeDescription.Generic.Builder.parameterizedType(ObjectEvent.class, clz).build();
 		
@@ -798,9 +797,6 @@ public class TemplateVersionServiceImpl extends AbstractLoggingServiceImpl imple
 				 }
 			 }
 			 
-			 b = b.method(named("async").and(takesNoArguments()).and(returns(boolean.class)))
-			 .intercept(FixedValue.value(async));
-			 
 			 b = b.defineConstructor(Visibility.PUBLIC)
 			  .withParameters(clz)
 			  .intercept(MethodCall
@@ -836,7 +832,7 @@ public class TemplateVersionServiceImpl extends AbstractLoggingServiceImpl imple
 	
 	@SuppressWarnings("unchecked")
 	private void generateUpdateEventTemplate(String className, String resourceKey, String bundle, String group, Class<?> clz, 
-			String eventKey, boolean newSchema, boolean audited, boolean async) {
+			String eventKey, boolean newSchema, boolean audited) {
 		
 		Generic genericType = TypeDescription.Generic.Builder.parameterizedType(ObjectUpdateEvent.class, clz).build();
 		
@@ -873,9 +869,6 @@ public class TemplateVersionServiceImpl extends AbstractLoggingServiceImpl imple
 					 b = b.annotateType(a);
 				 }
 			 }
-			 
-			 b = b.method(named("async").and(takesNoArguments()).and(returns(boolean.class)))
-					 .intercept(FixedValue.value(async));
 			 
 			 b = b.defineConstructor(Visibility.PUBLIC)
 			  .withParameters(clz, clz)
