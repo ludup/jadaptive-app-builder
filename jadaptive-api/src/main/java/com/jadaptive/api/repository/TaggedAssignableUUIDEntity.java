e73bf0f26826cb38cf05d41ab2cadef0b81b162d
Brett Smith <brett@logonbox.com>
2026-02-01 23:14:58 +0000

    Field is protected.

--- jadaptive-api/src/main/java/com/jadaptive/api/repository/TaggedAssignableUUIDEntity.java
+++ jadaptive-api/src/main/java/com/jadaptive/api/repository/TaggedAssignableUUIDEntity.java
@@ -15,7 +15,7 @@ public abstract class TaggedAssignableUUIDEntity extends AssignableUUIDEntity im
 
 	@ObjectField(searchable = true, unique = true, type = FieldType.TEXT, nameField = true, weight = 0)
 	@Validator(type = ValidationType.REQUIRED)
-	private String name;
+	protected String name;
 
 	@ObjectField(type = FieldType.TEXT, searchable = true, renderer = FieldRenderer.TAGS)
 	private Collection<String> tags;
