ca41b087dc1b8cb63ce2fa95fda41d845f1433dc
Brett Smith <brett@logonbox.com>
2026-02-12 11:21:03 +0000

    Broke old dropdown.

--- jadaptive-api/src/main/resources/com/jadaptive/api/ui/pages/ext/Footer.js
+++ jadaptive-api/src/main/resources/com/jadaptive/api/ui/pages/ext/Footer.js
@@ -3,7 +3,6 @@ $(function() {
 	const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 	const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-	/*
 	$(document).on('click', '[role=dropdown-menu-item-select]', function(e) {
 		e.preventDefault();
 		var sel = $(this);
@@ -25,7 +24,6 @@ $(function() {
 		$(this).closest(".dropdown").find('.dropdown-menu').removeClass('show');
 		$(this).closest(".dropdown").find('input[type="hidden"]').val($(this).data('resourcekey')).change();
 	});
-	*/
 
 	$(document).on('click', '.replacement-item', function(e) {
 		e.preventDefault();
@@ -34,7 +32,6 @@ $(function() {
 		$(this).closest(".input-group").find('.dropdown-menu').removeClass('show');
 	});
 
-	/*
 	$(document).on('click keyup', '.jdropdown', function(e) {
 		e.stopPropagation();
 		var el = $(this).closest(".dropdown");
@@ -43,7 +40,6 @@ $(function() {
 		const dropdownList = new bootstrap.Dropdown(dropdownToggleEl);
 		dropdownList.toggle();
 	});
-	*/
 	
 	function duplicate(text, list) {
 		var found = false;
@@ -323,7 +319,6 @@ $(function() {
 
 	});
 	
-	/*
 	$('.filter-dropdown').on('keypress', function(e) {
 		var text = $(this).val().trim().toLowerCase();
 		$(this).parent().find('.dropdown-menu a').each(function(idx, obj) {
@@ -350,7 +345,6 @@ $(function() {
 		}
 				
 	})
-	*/;
 
 	$('#footer input[name="theme"]').on('change', function(e) {
 		document.cookie = "userTheme=" + $(this).val() + '; path=/; expires=Tue, 01 Jan 2038 00:00:00 UTC;';
